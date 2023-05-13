7f553a3364bb1178cef31dcd32b218ded6af7496
Monny <magallaneskim774@gmail.com>
2023-05-13 15:47:37 +0800

    usestate practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import Board from './components/board'
-
+import { useState } from 'react';
 import './style.scss'
 
 function App() {
 
+  useState()
+
+  let counter =1;
+  const onBtnClick = () =>{
+    console.log('hello');
+    counter = counter +1;
+  };
+
   return (
     <div className='app'>
-      <Board />
+      <div>
+        <button onClick={onBtnClick}>
+          Click me please
+        </button>
+        <div>{counter}</div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App
